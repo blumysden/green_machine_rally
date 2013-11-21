b2593b830a87f885727741a2079104bb3acd4fda
Scott Blumenthal <scott.blumenthal@gmail.com>
2013-11-21 07:31:08 -0500

    Up to the timer

--- javascripts/main.js
+++ javascripts/main.js
@@ -33,7 +33,7 @@
       _.each(teams, function(team, i) {
         if (!this.racers.length) {
           var racers = _.shuffle(boys);
-          this.racers = (i == 0) ? racers : _.difference(racers, team[0]);
+          this.racers = (i === 0) ? racers : _.difference(racers, team[0]);
         }
         while (team.length < teamSize && this.racers.length) {
           team.push(this.racers.splice(0, 1)[0]);
@@ -41,13 +41,13 @@
       }, this);
       return teams;
     }
-  }
+  };
 
   var Race = function(type, teams) {
     this.type = type;
     this.teams = teams;
     this.$el = $();
-  }
+  };
 
   var $controls = $('#controls'),
       $type = $(':radio', $controls),
@@ -63,9 +63,14 @@
         case 'new-event':
           addEvent();
           break;
+        case 'new-racer':
+          addRacer();
+          break;
       }
     });
-    $eventArea.on('click', 'button', addRace);
+    $eventArea.
+      on('click', '#new-race', addRace).
+      on('click', '#timer', toggleTimer);
   }
 
   function addEvent() {
@@ -75,8 +80,27 @@
     $eventArea.html(e.$el);
   }
 
+  function addRacer() {
+    var $name = $(':text[name=new-racer-name]'),
+        name = $name.val();
+    if (name) {
+      boys.push(name);
+      $name.val('');
+    }
+  }
+
   function addRace() {
     events[events.length - 1].addRace();
+  }
+
+  function toggleTimer(e) {
+    var $button = $(e.target),
+        start = Date.parse(new Date());
+    if ($button.hasClass('start')) {
+      $button.removeClass('start').addClass('stop').text('STOP');
+    } else if ($button.hasClass('stop')) {
+      $button.removeClass('stop').text('DONE');
+    }
   }
 
   window.Rally = {
